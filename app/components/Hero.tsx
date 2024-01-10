7501c172c63ebd57546b2a782beb6e8af1e91cb5
Håkon Harnes <hakon@harnes.dev>
2024-01-10 22:04:01 +0100

    feat: add static terminal

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,25 +1,63 @@
 import Button from "./Button";
+import { Roboto_Mono } from "next/font/google";
+
+const roboto = Roboto_Mono();
 
 export default function Hero() {
   return (
-    <div className="mx-auto flex max-w-5xl flex-col gap-2 items-center justify-center pt-20 sm:pt-24 lg:pt-32">
-      <h1 className="text-4xl font-extrabold tracking-tight dark:text-white sm:text-5xl lg:text-6xl">
+    <div className="mx-auto flex flex-col items-center justify-between gap-10 pt-20 sm:pt-24 lg:pt-32 md:flex-row">
+      <AboutMe />
+      <Terminal />
+    </div>
+  );
+}
+
+function AboutMe() {
+  return (
+    <div className="flex flex-col items-center gap-5 md:items-start flex-1 md:max-w-xl">
+      <h1 className="text-4xl font-extrabold tracking-tight dark:text-white sm:text-4xl md:text-5xl lg:text-6xl">
         Håkon Harnes<span className="text-sky-400">.</span>
       </h1>
-      <h2 className="text-3xl font-medium tracking-tight dark:text-white sm:text-4xl lg:text-5xl">
-        [<span className="text-sky-400">developer</span>]
-      </h2>
-      {/* <p className="mx-auto my-6 max-w-3xl text-justify text-lg text-slate-200 "> */}
-      {/*   I’ve written a few thousand words on why traditional “semantic class */}
-      {/*   names” are the reason CSS is hard to maintain, but the truth is you’re */}
-      {/*   never going to believe. */}
-      {/* </p> */}
-      <div className="flex gap-5 pt-4">
+      <p className="text-justify text-xl text-slate-400 leading-6 md:max-w-md">
+        I&apos;m a <span className="text-sky-400 font-semibold">developer</span>{" "}
+        developer from Norway. I am a student at the Norwegian University of
+        Science and Technology. At the moment, I am studying.
+      </p>
+      <div className="flex gap-5 pt-2">
         <Button href="mailto:hakon@harnes.co">Contact me</Button>
         <Button href="/" color="white">
           Resume
         </Button>
       </div>
+    </div>
+  );
+}
+
+function Terminal() {
+  return (
+    <div
+      className={`${roboto.className} bg-slate-950 w-full text-lg p-6 rounded-lg flex-1 md:max-w-xl `}
+    >
+      <div>
+        <span className="">$</span> <span className="text-sky-200">echo</span>{" "}
+        <span className="">$name</span>
+      </div>
+      <div className="pb-4"> Håkon Harnes</div>
+      <div>
+        <span className="">$</span> <span className="text-sky-200">echo</span>{" "}
+        <span className="">$age</span>
+      </div>
+      <div className="pb-4">25</div>
+      <div>
+        <span className="">$</span> <span className="text-sky-200">echo</span>{" "}
+        <span className="">$location</span>
+      </div>
+      <div className="pb-4">Norway</div>
+      <div>
+        <span className="">$</span> <span className="text-sky-200">echo</span>{" "}
+        <span className="">$education</span>
+      </div>
+      <div className="pb-4">MSc. in Computer Science at NTNU</div>
     </div>
   );
 }
