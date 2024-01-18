bae0662261ffe70db2ee33c6f9709bf89cf30014
Håkon Harnes <hakon@harnes.dev>
2024-01-18 14:58:32 +0100

    fix: footer text color

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <section className="flex justify-center py-5 text-slate-300 mt-28">
+    <section className="flex justify-center py-5 text-slate-400 mt-28">
       © Håkon Harnes 2024
     </section>
   );
