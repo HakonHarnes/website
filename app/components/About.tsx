5d960ee73c64076115fed42eacf7cfa9d918833b
Håkon Harnes <hakon@harnes.dev>
2024-01-18 13:06:47 +0100

    chore: justify about me text

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -7,7 +7,7 @@ export default function About() {
     <section>
       <SectionHeader title="About" />
       <div className="flex flex-col gap-8 lg:flex-row lg:gap-28">
-        <div className="flex flex-col gap-4 text-lg text-slate-400">
+        <div className="flex flex-col gap-4 text-lg text-slate-400 text-justify">
           <p>
             Lorem ipsum odor amet, consectetuer adipiscing elit. Pretium rhoncus
             inceptos sagittis sociosqu; molestie nostra nam. Conubia nisi
