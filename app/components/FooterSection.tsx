cc91ec14d85c4d5431a30105ba647abf5a75209a
Håkon Harnes <hakon@harnes.dev>
2024-01-19 23:01:21 +0100

    chore: adjust footer padding

--- app/components/FooterSection.tsx
+++ app/components/FooterSection.tsx
@@ -1,6 +1,6 @@
 export default function FooterSection() {
   return (
-    <section className="flex justify-center py-5 text-slate-400 mt-28">
+    <section className="flex justify-center py-5 text-slate-400 mt-10">
       © Håkon Harnes 2024
     </section>
   );
