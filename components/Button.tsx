3ae49f5b24e993259e88aefed322d7561b115641
Håkon Harnes <hakon@harnes.dev>
2024-03-14 11:51:04 +0100

    chore: remove unused button style

--- components/Button.tsx
+++ components/Button.tsx
@@ -12,8 +12,6 @@ function buttonColorClasses(color: ButtonColors) {
   switch (color) {
     case "white":
       return "bg-white hover:bg-gray-300 text-black";
-    case "red":
-      return "bg-red-500 hover:bg-red-400 text-white";
     case "sky":
     default:
       return "bg-sky-500 hover:bg-sky-400 text-white";
