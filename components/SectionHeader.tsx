b897e33f73731eb5c85cbffffe87f252b9f04e86
Håkon Harnes <hakon@harnes.dev>
2024-03-14 11:55:07 +0100

    feat: make section lines more visible

--- components/SectionHeader.tsx
+++ components/SectionHeader.tsx
@@ -11,7 +11,7 @@ export const SectionHeader = ({ title, dir = "l" }: Props) => {
       }`}
       id={title.toLowerCase()}
     >
-      <div className="w-full h-[2px] bg-slate-700 opacity-30" />
+      <div className="w-full h-[3px] bg-slate-600 opacity-30" />
       <h2 className="text-right text-3xl lg:text-4xl font-extrabold">
         <span>{title}</span>
       </h2>
