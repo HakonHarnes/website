1c6b0c75560d39709c5165232572d4f1de1d507e
Håkon Harnes <hakon@harnes.dev>
2024-04-25 15:00:27 +0200

    fix: broken project does not exits page

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -25,14 +25,14 @@ export default function Page({ params }: ProjectProps) {
 
 function ProjectDoesNotExist() {
   return (
-    <section className="flex h-full flex-col items-center justify-center gap-6 py-20 sm:py-24 lg:py-32">
+    <section className="flex h-full flex-col items-center text-center justify-center gap-6 py-20 sm:py-24 lg:py-32">
       <h1 className="text-4xl font-extrabold tracking-tight dark:text-white md:text-4xl lg:text-6xl">
         Project does not exist (yet)
       </h1>
-      <p className="text-xl text-slate-400 lg:text-2xl">
-        But maybe I can help you with your project?
+      <p className="text-xl text-slate-400 lg:text-2xl font-medium">
+        Maybe I can help you with your project?
       </p>
-      <div className="flex gap-3 lg:gap-4">
+      <div className="flex gap-3 lg:gap-4 w-80">
         <Button href="mailto:hakon@harnes.co">Contact me</Button>
         <Button href="/" color="white">
           Go back
